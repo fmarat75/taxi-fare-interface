47c5f974c9b5115716bc16016550e5bf3d39380f
Felix Maratier <felixmaratier@gmail.com>
2024-03-01 11:15:32 +0100

    key change

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZm1hcmF0NzUiLCJhIjoiY2x0OGg5Y203MGt6eDJvcng5cjU5NWx3NyJ9.3M69P8Ize8R_Ajxz7Y3kIg';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
